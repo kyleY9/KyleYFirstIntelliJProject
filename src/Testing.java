49954d45419e2ec2f700bcbd606b4a7cd1a4a30d
KyleY9 <KyleY9@nycstudents.net>
2023-10-02 12:32:34 -0400

    more testing

--- src/Testing.java
+++ src/Testing.java
@@ -9,8 +9,24 @@ public class Testing {
         DecimalFormat df = new DecimalFormat("#.##");
 
         // prints
-        System.out.println("Gimme a decimal number: ");
-        double deci = scan.nextDouble();
-        System.out.println(df.format(deci) + " is your rounded decimal");
+        System.out.print("GIVE MATHEMATICAL OPERATOR: ");
+        String operator = scan.nextLine();
+        System.out.print("GIVE ME A NUMBER NOW!: ");
+        double deci1 = scan.nextDouble();
+        System.out.print("ANOTHER ONE, NOW!: ");
+        double deci2 = scan.nextDouble();
+
+        // when testing a string for equality, do not use "==", use the function (x.equals(y))
+        if (operator.equals("+")) {
+            System.out.println(df.format(deci1 + deci2));
+        } else if (operator.equals("-")) {
+            System.out.println(df.format(deci1 - deci2));
+        } else if (operator.equals("*")) {
+            System.out.println(df.format(deci1 * deci2));
+        } else if (operator.equals("/")) {
+            System.out.println(df.format(deci1 / deci2));
+        } else {
+            System.out.println("Give me a valid operator >:<");
+        }
     }
 }
